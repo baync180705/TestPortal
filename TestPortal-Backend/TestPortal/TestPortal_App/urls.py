07a33b1c31f79949dbc3f13ed2d4f5b1af0f73d9
username <kindle.maheep@gmail.com>
2024-04-13 00:00:55 +0530

    feat:minor changes to views

--- TestPortal-Backend/TestPortal/TestPortal_App/urls.py
+++ TestPortal-Backend/TestPortal/TestPortal_App/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from .views import question,createStudent,login
+from .views import question,createStudent,login,makeAQuiz
 from TestPortal_App import views
 urlpatterns=[
     path("question",question.as_view()),
     path("studentSignUp",createStudent.as_view()),
-    path("login",login.as_view())
+    path("login",login.as_view()),
+    path("quiz",makeAQuiz.as_view())
 ]
